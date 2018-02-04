c74af052bb8bbe1b4e4ae6540f3e61223ac0954b
Danilo Souza <danilos063@gmail.com>
2018-02-03 19:36:33 -0500

    Applied Required Attributes and Operations
    Implemented: Constructor, getPath()
    Created Variables: scenarioNumber, and path

--- Game/src/sample/MapTransition.java
+++ Game/src/sample/MapTransition.java
@@ -1,4 +1,20 @@
+//Members Responsible: Callum, Daniel, Danilo
+//Exists to hold the required information so that a Map Transition can occur
+
 package sample;
 
-public class MapTransition {
+public class MapTransition extends tileObject{
+	private int scenarioNumber;
+	private String path;
+	
+	//Sets Default values for the global variables
+	public MapTransition(String path) {
+		scenarioNumber = 0;
+		this.path = path; 
+	}
+	
+	//Returns a string with the path of the Map file to be loaded
+	public String getPath(){
+		return this.path;
+	}
 }
