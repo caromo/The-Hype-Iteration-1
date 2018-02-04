e48ed0d448b9b99d44c38db19ab5f43e1083137a
Danilo Souza <danilos063@gmail.com>
2018-02-03 19:18:22 -0500

    Added Required Operations and Attributs
    Added required operations: scenarioNumber, duration, constructor, and getDuration()

--- Game/src/sample/AreaEffect.java
+++ Game/src/sample/AreaEffect.java
@@ -1,4 +1,20 @@
+//Exists to hold information for the Area Effects that will happen in the tiles
+//Members responsible: Callum, Daniel, Danilo
+
 package sample;
 
-public class AreaEffect {
+public class AreaEffect extends tileObject{
+  private int scenarioNumber;
+  private float duration;
+
+  //Sets the default values for the global variables
+  public AreaEffect(){
+    duration = 10f;
+    scenarioNumber = 0;
+  }
+
+  //Allows for us to use durationin other classes while keeping it private
+  public float getDuration(){
+    return this.duration;
+  }
 }
