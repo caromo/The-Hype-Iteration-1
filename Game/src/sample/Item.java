4696978d58c321f15fe4c88dae578f9fd84e0726
greenphantom <daniel.tymecki@gmail.com>
2018-02-10 12:56:08 -0500

    Added One-Shot Functionality
    Alos fixed heal bug with level one player

--- Game/src/sample/Item.java
+++ Game/src/sample/Item.java
@@ -2,14 +2,14 @@ package sample;
 
 public class Item extends tileObject{
     private int itemID;
+    private boolean oneShot;
     public int ScenarioID;
 
     public Item(int SN, int id) {
         super(SN);
         itemID = id;
+        oneShot = false;
     }
-
-    public void useItem() {  }
 
     public int getID() {
         return itemID;
@@ -25,6 +25,14 @@ public class Item extends tileObject{
     }
 
     public void use() {}
+
+    public boolean isOneShot() {
+        return oneShot;
+    }
+
+    public void setOneShot(boolean oneShot) {
+        this.oneShot = oneShot;
+    }
 
 //    @Override
 //    public int getEffect() {
