410a9dd0fd3e08a201139f58a63a13cc1175a9a6
Carlo Romo <delebot@gmail.com>
2018-02-10 15:56:12 -0500

    fleshed out map

--- Game/src/sample/Map.java
+++ Game/src/sample/Map.java
@@ -2,16 +2,22 @@ package sample;
 
 import java.awt.Point;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+
+import static java.lang.Character.isLetter;
 
 public class Map {
-    private Tile[][] tileSet;
+    private tile[][] tileSet;
     private Player myPlayer;
     private Point startingPoint;
+    private int mapID;
 
     //generic map constructor: makes an empty 10x10 grid
-    public Map() {
-        myPlayer = new Player();
-        tileSet = new Tile[10][10];
+    public Map(Player player) {
+        myPlayer = player;
+        tileSet = new tile[10][10];
     }
     //map constructor: sets map to one specified by map ID and places player at the starting point
     public Map(int mapID, Player currPlayer) {
@@ -24,7 +30,7 @@ public class Map {
         myPlayer.setPosition((int)startingPoint.getX(), (int)startingPoint.getY());
     }
 
-    public movePlayer(Point dir) {
+    public void movePlayer(Point dir) {
         if ( tileSet[(int)dir.getX()][(int)dir.getY()].getPassable() ) {
             myPlayer.setPosition( (int)dir.getX(), (int)dir.getY() );
             tileSet[(int)dir.getX()][(int)dir.getY()].applyEffect();
@@ -33,51 +39,73 @@ public class Map {
     public void updateMap(int mapID) {
         loadMapFromID(mapID);
         moveToStart();
-    } 
+    }
     public Player getPlayer() {
         return myPlayer;
     }
-    public Tile[][] getMap() {
+    public tile[][] getMap() {
         return tileSet;
     }
 
-    private void loadMapFromID(int mapID) {
-        File mapFile = new File(System.getProperty("user.dir") + "/Save/Map/" + mapID +".txt");
-        BufferedReader br_map = new BufferedReader(new FileReader(mapFile));
-        Scanner s_map = new Scanner(br_map.readLine());
-        String sMapSizeX = s_map.next();
-        String sMapSizeY = s_map.next();
-        int mapSizeX = Integer.parseInt(sMapSizeX);
-        int mapSizeY = Integer.parseInt(sMapSizeY);
+    public void loadMapFromID(int mapID) {
 
-        s_map = new Scanner(br_map.readLine());
-        Point newStart = new Point(Integer.parseInt(s_map.next()), Integer.parseInt(s_map.next()));
-        startingPoint = newStart;
+        try {
+            File mapFile = new File(System.getProperty("user.dir") + "/Save/Map/" + mapID + ".txt");
+            BufferedReader br_map = new BufferedReader(new FileReader(mapFile));
+            Scanner s_map = new Scanner(br_map.readLine());
+            String sMapSizeX = s_map.next();
+            String sMapSizeY = s_map.next();
+            int mapSizeX = Integer.parseInt(sMapSizeX);
+            int mapSizeY = Integer.parseInt(sMapSizeY);
 
-        tileSet = new Tile[mapSizeX][mapSizeY];
+            s_map = new Scanner(br_map.readLine());
+            Point newStart = new Point(Integer.parseInt(s_map.next()), Integer.parseInt(s_map.next()));
+            startingPoint = newStart;
 
-        for (int i = 0; i < mapSizeX; i++) {
-            String st = br_map.readLine();
-            Scanner scanInput = new Scanner(st);
-        
-            for (int j = 0; j < mapSizeY; j++) {
-                String temp = scanInput.next();
-        
-                if (!isLetter(temp.charAt(0))){
-                    String equid = "" + temp.charAt(0) + temp.charAt(1) + temp.charAt(2);
-                    String data = "" + temp.charAt(3) + temp.charAt(4);
-                    // System.out.println(equid + "" + data);      //testing
-                    tileSet[i][j] = new tile();
-                    tileSet[i][j].fill('g', 6, Integer.parseInt(equid), Integer.parseInt(data) );
-                }
-                else {
-                    //System.out.println(temp.charAt(0)+ "" + temp.charAt(1) +"" + temp.charAt(2) + "" + temp.charAt(3) + "" +temp.charAt(4));    //testing
-                    tileSet[i][j] = new tile();
-                    tileSet[i][j].fill(temp.charAt(0), (int) temp.charAt(1), (int) temp.charAt(2), 0);
+            tileSet = new tile[mapSizeX][mapSizeY];
+
+            for (int i = 0; i < mapSizeX; i++) {
+                String st = br_map.readLine();
+                Scanner scanInput = new Scanner(st);
+
+                for (int j = 0; j < mapSizeY; j++) {
+                    String temp = scanInput.next();
+
+                    if (!isLetter(temp.charAt(0))) {
+                        String equid = "" + temp.charAt(0) + temp.charAt(1) + temp.charAt(2);
+                        String data = "" + temp.charAt(3) + temp.charAt(4);
+                        // System.out.println(equid + "" + data);      //testing
+                        tileSet[i][j] = new tile();
+                        tileSet[i][j].fill('g', 6, Integer.parseInt(equid), Integer.parseInt(data));
+                    } else {
+                        //System.out.println(temp.charAt(0)+ "" + temp.charAt(1) +"" + temp.charAt(2) + "" + temp.charAt(3) + "" +temp.charAt(4));    //testing
+                        tileSet[i][j] = new tile();
+                        tileSet[i][j].fill(temp.charAt(0), (int) temp.charAt(1), (int) temp.charAt(2), 0);
+                    }
                 }
             }
+            s_map.close();
+            br_map.close();
+        }catch(Exception e){
+            e.printStackTrace();
         }
-        s_map.close();
-        br_map.close();
     }
+
+    public int getMapID() {
+        return mapID;
+    }
+
+    public void setMapID(int newID) {
+        mapID = newID;
+    }
+
+    public int getMapX() {
+        return tileSet.length;
+    }
+
+    public int getMapY() {
+        return tileSet[0].length;
+    }
+
+    public tile[][] getState() { return tileSet; }
 }
