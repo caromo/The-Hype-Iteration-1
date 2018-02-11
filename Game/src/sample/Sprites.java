06f915b4d38c09b998842c4aaa76bd5177fdab9b
Carlo Romo <delebot@gmail.com>
2018-02-11 17:13:22 -0500

    fff

--- Game/src/sample/Sprites.java
+++ Game/src/sample/Sprites.java
@@ -16,7 +16,7 @@ public class Sprites {
     private String workingDir;
     public Sprites() {
 
-        workingDir = System.getProperty("user.dir");
+        workingDir = System.getProperty("user.dir")+"/Game";
 
         //System.out.println(workingDir);
         itemSprites = new ArrayList<Image>();
@@ -42,10 +42,10 @@ public class Sprites {
         AESprites.add(getImage(workingDir + "/src/sample/sprites/health.png"));
         AESprites.add(getImage(workingDir + "/src/sample/sprites/star.png"));
 
-        playerSprites.add(getImage(System.getProperty("user.dir") + "/Game/src/sample/sprites/characterGuy.png"));
-        playerSprites.add(getImage(System.getProperty("user.dir") + "/Game/src/sample/sprites/characterGirl.png"));
-        playerSprites.add(getImage(System.getProperty("user.dir") + "/Game/src/sample/sprites/characterAdventurer.png"));
-        playerSprites.add(getImage(System.getProperty("user.dir") + "/Game/src/sample/sprites/characterSoldier.png"));
+        playerSprites.add(getImage(System.getProperty("user.dir") + "/src/sample/sprites/characterGuy.png"));
+        playerSprites.add(getImage(System.getProperty("user.dir") + "/src/sample/sprites/characterGirl.png"));
+        playerSprites.add(getImage(System.getProperty("user.dir") + "/src/sample/sprites/characterAdventurer.png"));
+        playerSprites.add(getImage(System.getProperty("user.dir") + "/src/sample/sprites/characterSoldier.png"));
 
         mapTransitionSprite = getImage(workingDir + "/src/sample/sprites/door.png");
 
