477184c30300516f2bd0c12942ba9cde8e5fa365
Carlo Romo <delebot@gmail.com>
2018-02-11 14:30:28 -0500

    Added Map Transition

--- Game/src/sample/Map.java
+++ Game/src/sample/Map.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.Scanner;
+import java.io.PrintWriter;
 import static java.lang.Character.isLetter;
 
 /*
@@ -35,23 +36,52 @@ public class Map {
     //function to place player at the beginning point of a specific map
     private void moveToStart() {
         myPlayer.setPosition((int)startingPoint.getX(), (int)startingPoint.getY());
+        tileSet[(int)startingPoint.getX()][(int)startingPoint.getY()].occupy = new Occupy(myPlayer, tileSet[(int)startingPoint.getX()][(int)startingPoint.getY()]);
     }
 
     //moves the player and resolves tile event (If passable)
     public void movePlayer(Point dir) {
         if ( tileSet[(int)dir.getX()][(int)dir.getY()].getPassable() ) {
-            tileSet[(int)myPlayer.getPosition().getX()][(int)myPlayer.getPosition().getY()].cancelEffect();
-            tileSet[(int)myPlayer.getPosition().getX()][(int)myPlayer.getPosition().getY()].occupy = null;
             tileSet[(int)dir.getX()][(int)dir.getY()].occupy = new Occupy(myPlayer, tileSet[(int)dir.getX()][(int)dir.getY()]);
             myPlayer.setPosition( (int)dir.getX(), (int)dir.getY() );
-            tileSet[(int)dir.getX()][(int)dir.getY()].applyEffect();
+            updateMap(tileSet[(int)dir.getX()][(int)dir.getY()].applyEffect());
         }
     }
 
     //loads a new tileset and places the player in the starting point
     public void updateMap(int mapID) {
+        if (mapID == -1) return;
+
         loadMapFromID(mapID);
         moveToStart();
+    }
+
+    public void saveMap() {
+        int currID = mapID;
+        int mapSizeX = tileSet.length;
+        int mapSizeY = tileSet[0].length;
+
+        try {
+            //opens the map file based on MapID for writing
+            PrintWriter pw = new PrintWriter(System.getProperty("user.dir") + "/Save" + "/Map" + "/" + Integer.toString(mapID)
+                    + ".txt");
+            //write first line map size: X Y
+            pw.println(mapSizeX + " " + mapSizeY);
+            //write second line with startingPosition
+            pw.println((int)myPlayer.getPosition().getX() + " " + (int)myPlayer.getPosition().getY());
+
+            //Iterating through the map printing tiles in 5 digits
+            for (int i = 0; i < mapSizeX; i++) {
+                for (int j = 0; j < mapSizeY; j++) {
+                    pw.print(tileSet[i][j].spill());
+                    pw.print(" ");
+                }
+                pw.println();
+            }
+            pw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     //given an ID, loads a map text file with tileset and starting point information
@@ -66,6 +96,7 @@ public class Map {
             int mapSizeX = Integer.parseInt(sMapSizeX);
             int mapSizeY = Integer.parseInt(sMapSizeY);
 
+            saveMap();
             s_map = new Scanner(br_map.readLine());
             Point newStart = new Point(Integer.parseInt(s_map.next()), Integer.parseInt(s_map.next()));
             startingPoint = newStart;
@@ -82,11 +113,9 @@ public class Map {
                     if (!isLetter(temp.charAt(0))) {
                         String equid = "" + temp.charAt(0) + temp.charAt(1) + temp.charAt(2);
                         String data = "" + temp.charAt(3) + temp.charAt(4);
-                        // System.out.println(equid + "" + data);      //testing
                         tileSet[i][j] = new tile();
                         tileSet[i][j].fill('g', 6, Integer.parseInt(equid), Integer.parseInt(data));
                     } else {
-                        //System.out.println(temp.charAt(0)+ "" + temp.charAt(1) +"" + temp.charAt(2) + "" + temp.charAt(3) + "" +temp.charAt(4));    //testing
                         tileSet[i][j] = new tile();
                         tileSet[i][j].fill(temp.charAt(0), (int) temp.charAt(1)-48, (int) temp.charAt(2)-48, 0);
                     }
